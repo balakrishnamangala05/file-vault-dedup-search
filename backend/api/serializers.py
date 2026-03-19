8773b26870f4e71f7fcd948223962d299d0ad5f3
Balakrishna Mangala <balakrishnamangala05@gmail.com>
2026-03-19 18:10:53 -0400

    added TagSerializer and exposed tags list in FileUploadSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
-from .models import StoredFile, FileUpload
+from .models import StoredFile, FileUpload, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ["id", "name", "color"]
 
 
 class FileUploadSerializer(serializers.ModelSerializer):
     stored_file = serializers.SerializerMethodField()
+    tags = TagSerializer(many=True, read_only=True)
 
     class Meta:
         model = FileUpload
-        fields = ["id", "original_name", "uploaded_at", "stored_file"]
+        fields = ["id", "original_name", "uploaded_at", "stored_file", "tags"]
 
     def get_stored_file(self, obj):
         return StoredFileFlatSerializer(obj.stored_file).data
